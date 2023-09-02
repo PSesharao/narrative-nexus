5bbecc5a792ce5f504ae15090af8ad3d0f0abdd7
PSesharao <paritalasesharao@gmail.com>
2023-09-02 23:59:02 +0530

    handle error response from Open AI API on Sat, Sep 02, 2023  11:59:03 PM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {  Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +15,16 @@ export class AppComponent {
   ]
 
   title = 'narrative-nexus';
+
+  currentAction : any ; 
+
+  constructor(private router : Router){
+
+  }
+
+  handleRoute(action : any ) {
+    this.currentAction = action ; 
+    this.router.navigateByUrl(action.route ) ; 
+  }
+
 }
--- src/app/gpt/gpt.component.ts
+++ src/app/gpt/gpt.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-gpt',
@@ -7,4 +9,51 @@ import { Component } from '@angular/core';
 })
 export class GptComponent {
 
+  queryformGroup!: FormGroup;
+
+  result: any;
+
+  error: any;
+
+  "messages": [{ "role": "system", "content": "You are a helpful assistant." }];
+
+  constructor(private fb: FormBuilder,
+    private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    this.queryformGroup = this.fb.group({
+      query: this.fb.control("")   // Initializing query with empty string first
+    });
+  }
+
+
+  handleAskGPT() {
+
+    let url = "https://api.openai.com/v1/chat/completions";
+
+    let httpHeaders = new HttpHeaders().
+      set("Authorization", "Bearer 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")
+      .set("OpenAI-Organization", "org-UQ2KEcCmzPxDNFK0zG86zVPn")
+
+    let payload = {
+
+      "model": "gpt-3.5-turbo",
+      "messages": this.messages
+
+    };
+
+    this.httpClient.post(url, payload, { headers: httpHeaders })
+      .subscribe(
+        (resp) => {
+          this.result = resp;
+        },
+        (err) => {
+          this.error = err.error["error"]["message"]
+          console.log(err.error["error"])
+        }
+      );
+  }
 }
+
+
+
